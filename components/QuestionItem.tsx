33fdc8c0babdbf90e14de7a886e06452ad2d92b0
Francisco Casanova <francisco_casanova@hotmail.es>
2025-11-25 18:52:30 -0300

    feature: complete list's responsive design

--- components/QuestionItem.tsx
+++ components/QuestionItem.tsx
@@ -18,7 +18,10 @@ const QuestionItem: React.FunctionComponent<IQuestionProps> = async (props) => {
   const difficulty = difficulties.find(difficulty => difficulty.id === props.difficulty)
 
   return (
-    <li className='grid grid-flow-col auto-cols-fr justify-center
+    <li className='
+      grid grid-flow-row grid-cols-1
+      sm:grid-cols-2 sm:grid-rows-4
+      md:grid-cols-18 md:grid-rows-1
       even:bg-gray-100 odd:bg-gray-200
       dark:even:bg-gray-700 dark:odd:bg-gray-800
       hover:bg-blue-200 hover:dark:bg-blue-950
@@ -27,8 +30,13 @@ const QuestionItem: React.FunctionComponent<IQuestionProps> = async (props) => {
       scale-100 hover:scale-105
       shadow-none hover:shadow-lg dark:shadow-gray-950
     '>
-      <span className='font-bold col-span-4'>{props.qid}. {props.title}</span>
-      <div className='flex items-start col-span-3'>
+      <span className='font-bold md:col-span-4'>{props.qid}. {props.title}</span>
+      <div className='
+        flex items-start space-x-3
+        sm:col-start-2
+        md:col-span-3 md:col-start-auto
+      '>
+        <span className='md:hidden'>Difficulty:</span>
         <span className={`
           col-span-2
           ${DIFFICULTY_COLORS[(difficulty?.id || 1) - 1]}
@@ -38,7 +46,11 @@ const QuestionItem: React.FunctionComponent<IQuestionProps> = async (props) => {
           {difficulty?.value}
         </span>
       </div>
-      <div className='flex items-start col-span-3'>
+      <div className='
+        flex items-start space-x-3
+        sm:col-start-2 md:col-span-3 md:col-start-auto
+      '>
+        <span className='md:hidden'>Type:</span>
         <span className={`
           ${PILL_COLORS[type?.id || 0]}
           px-3 rounded-full text-center wrap-anywhere
@@ -46,9 +58,32 @@ const QuestionItem: React.FunctionComponent<IQuestionProps> = async (props) => {
           {props.type}
         </span>
       </div>
-      <span>{props.sum}</span>
-      <span className='col-span-3'>{props.question_summary}</span>
-      <a className='col-span-2 wrap-anywhere text-blue-400 visited:text-purple-400' href={`https://www.linkedin.com/search/results/companies/?keywords=${props.company_asked}`} target='_blank'>{props.company_asked}</a>
+      <div className='
+        flex items-start space-x-3
+        sm:col-start-2
+        md:col-start-auto
+      '>
+        <span className='md:hidden'>Votes:</span>
+        <span>{props.sum}</span>
+      </div>
+      <span className='
+        col-start-1 row-start-2 pb-12
+        sm:row-start-2 sm:row-span-3 sm:pb-0
+        md:col-span-4 md:col-start-auto md:row-start-auto md:row-span-auto'
+      >
+        {props.question_summary}
+      </span>
+      <a className='
+          wrap-anywhere text-blue-400
+          visited:text-purple-400
+          sm:col-start-2
+          md:col-span-3 md:col-start-auto
+        '
+        href={`https://www.linkedin.com/search/results/companies/?keywords=${props.company_asked}`}
+        target='_blank'
+      >
+        {props.company_asked}
+      </a>
     </li>
   );
 };
